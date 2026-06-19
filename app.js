5cceae61ea6743c22e24b2ad4741667ccc8767dc
PeterAdedokun1 <210409509@live.unilag.edu.ng>
2026-06-19 12:26:59 +0100

    Add security middleware to app.js
    Wire up helmet, cors, xss-clean, express-mongo-sanitize, and rate limiter (60 req/15 min)

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSantinize = require("express-mongo-sanitize");
 
 //DATEBASE
 const connectDB = require("./db/connect");
@@ -23,13 +28,26 @@ const notFoundMiddleWare = require("./middleware/not-found");
 const errorHandlerMiddleWare = require("./middleware/error-handler");
 const { authenticateUser } = require("./middleware/authentication");
 
+
+
+app.set("trust proxy",1);
+app.use(rateLimiter({
+  windowMs: 15 *60*1000,
+  max: 60
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss());
+app.use(mongoSantinize())
+
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
 app.get("/", (req, res) => {
-  res.send("here");
+  res.send("home page");
 });
 
 app.use("/api/v1/auth", authRouter);
